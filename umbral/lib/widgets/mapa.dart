ed7a7cc6dd8f8fb788fd307e35061f0bdc1c9ee8
AxdaTFK <axldug5@gmail.com>
2026-05-26 17:26:44 -0500

    FramesHechos

--- umbral/lib/widgets/mapa.dart
+++ umbral/lib/widgets/mapa.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import '../main.dart';
 
-class Videos extends StatelessWidget {
-  const Videos({super.key});
+class Mapa extends StatelessWidget {
+  const Mapa({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +16,7 @@ class Videos extends StatelessWidget {
 
       body: const Center(
         child: Text(
-          "Pantalla Videos",
+          "Mapa",
           style: TextStyle(
             color: Colors.white,
             fontSize: 24,
