718831465deafca079be3427f76b1420f7da5e09
AxdaTFK <axldug5@gmail.com>
2026-05-26 16:34:49 -0500

    Update main.dart

--- umbral/lib/main.dart
+++ umbral/lib/main.dart
@@ -9,11 +9,38 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
+        body: const Center(
           child: Text('Home'),
         ),
+
+        bottomNavigationBar: Bottombar(),
+      ),
+    );
+  }
+}
+
+class Bottombar extends StatelessWidget {
+  const Bottombar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 80,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+      color: Colors.black,
+
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Icon(Icons.home, color: Colors.white, size: 32),
+          Icon(Icons.search, color: Colors.white, size: 32),
+          Icon(Icons.favorite, color: Colors.white, size: 32),
+          Icon(Icons.person, color: Colors.white, size: 32),
+          Icon(Icons.settings, color: Colors.white, size: 32),
+        ],
       ),
     );
   }
